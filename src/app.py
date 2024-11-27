e6b152b32a1b86d8885b9490fe08c2bee9e6c799
Felipe Coste <32721311+felipecoste@users.noreply.github.com>
2024-11-27 19:08:51 +0000

    mehtod PUT agregado

--- src/app.py
+++ src/app.py
@@ -3,18 +3,23 @@ app = Flask(__name__)
 
 
 todos = [
-    {"label": "My first task", 
-    "done": False },
-    {"label": "Sample Todo 1",
-     "done": True}
+    {"id": 1, "label": "Tarea num 1", "done": False},
+    {"id": 2, "label": "Tarea num 2", "done": True}
 ]
 
 
 
 @app.route('/todos', methods=['GET'])
-def hello_world():
+def get_todos():
     json_text = jsonify(todos)
     return json_text
+
+#GET por id
+@app.route('/todos/<int:id>', methods=['GET'])
+def get_todo_id(id):
+    int_id = list(filter(lambda todo: todo['id'] == id, todos))
+    print(int_id)
+    return int_id
 
 @app.route('/todos', methods=['POST'])
 def add_new_todo():
@@ -23,11 +28,23 @@ def add_new_todo():
     print("Incoming request with the following body", request_body)
     return jsonify(todos)
 
-@app.route('/todos/<int:position>', methods=['DELETE'])
-def delete_todo(position):
-    del todos[position]
-    print("This is the position to delete:", position)
+@app.route('/todos/<int:id>', methods=['DELETE'])
+def delete_todo(id):
+    del todos[id]
+    print("This is the id to delete:", id)
     return jsonify(todos)
+
+@app.route('/todos/<int:id>', methods=['PUT'])
+def update_todo_id(id):
+    todo = next((todo for todo in todos if todo["id"] == id), None)
+    if not todo:
+        return jsonify({"error": "Todo not found"}), 404
+    request_body = request.get_json()
+    if "label" in request_body:
+        todo["label"] = request_body["label"]
+    if "done" in request_body:
+        todo["done"] = request_body["done"]
+    return jsonify(todo)
 
 
 if __name__ == '__main__':
